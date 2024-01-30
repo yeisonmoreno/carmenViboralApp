003d48377a244a4916756d9f56e175a0037acdbd
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-01-30 07:52:57 -0500

    se crea home de la app

--- app/src/main/java/com/example/sanrafa/Home.java
+++ app/src/main/java/com/example/sanrafa/Home.java
@@ -2,13 +2,57 @@ package com.example.sanrafa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
+
+    //ATRIBUTOS PARA REFERENCIAS ELEMENTOS GRAFICOS
+    //QUE TENGO YO EN EL XML QUE QUIERO CONTROLAR EN JAVA
+    Button botonHoteles;
+    Button botonRestaurantes;
+    Button botonSitios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //asociar las variables creadas con los ID de los
+        //elementos que vienen desde XML
+        botonHoteles=findViewById(R.id.botonhoteles);
+        botonRestaurantes=findViewById(R.id.botonrestaurantes);
+        botonSitios=findViewById(R.id.botonsitios);
+
+        //Escuchar eventos (clic en los botones)
+        botonHoteles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //aca escribo lo que quiera hacer cuando presionen el boton
+                //Toast.makeText(Home.this, "hizo clic en hoteles", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Home.this,Hoteles.class);
+                startActivity(intent);
+            }
+        });
+
+        botonRestaurantes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Home.this, "Hizo click en el Restaurante", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        botonSitios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Home.this, "hizo clic en Sitios Turisticos", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
     }
 }
